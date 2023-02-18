fcbc5b78a17241a3815f889db24a570c2f5213d2
rtungol <rarrtungol@gmail.com>
2023-02-17 21:01:56 -0500

    adding product-box component within home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { ProductsHeaderComponent } from "./pages/home/components/products-header/products-header.component";
 import { FiltersComponent } from "./pages/home/components/filters/filters.component";
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { FiltersComponent } from "./pages/home/components/filters/filters.compon
     HomeComponent,
     ProductsHeaderComponent,
     FiltersComponent,
+    ProductBoxComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
   cols = 3;
   category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols]
 
   constructor() {}
 
@@ -14,9 +15,10 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
-  onShowCategory(newCategory: string): void { 
+  onShowCategory(newCategory: string): void {
     this.category = newCategory;
   }
 }
