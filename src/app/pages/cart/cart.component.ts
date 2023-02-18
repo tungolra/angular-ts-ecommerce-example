0dedde9e6c92f91256fa6f8c83e34ec5dde217f9
rtungol <rarrtungol@gmail.com>
2023-02-17 22:56:30 -0500

    add functionality to reduce quantities of items on cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -49,4 +49,10 @@ export class CartComponent implements OnInit {
     this.cartService.removeFromCart(item);
   }
 
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, filter } from "rxjs";
 import { Cart, CartItem } from "../models/cart.model";
 
 @Injectable({
@@ -32,11 +32,32 @@ export class CartService {
     this.cart.next({ items: [] });
     this._snackBar.open("Cart cleared", "Ok", { duration: 3000 });
   }
-  removeFromCart(item: CartItem): void {
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
     const filteredItems = this.cart.value.items.filter(
       (_item) => _item.id !== item.id
     );
+
+    if (update) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open("Item removed from cart", "Ok", { duration: 3000 });
+    }
+    return filteredItems;
+  }
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+      return _item;
+    });
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
+    }
     this.cart.next({ items: filteredItems });
-    this._snackBar.open("Item removed from cart", "Ok", { duration: 3000 });
+    this._snackBar.open("1 Item removed from cart", "Ok", { duration: 3000 });
   }
 }
